b9bebaf0433a3e16a2000b5b616bbdb966635a6e
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-03-27 13:14:51 +0100

    first commit de l'architecture de l'app

---
+++ BackEnd/MMIPlatform/src/test/java/MMIPlatform/MMIPlatform/MmiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package MMIPlatform.MMIPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
