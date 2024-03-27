298d032ebfdb048fad1671fd5912efdb7b669f60
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-03-27 14:44:10 +0100

    envoie

---
+++ BackEnd/MMIPlatform/src/main/java/MMIPlatform/MMIPlatform/MmiPlatformApplication.java
@@ -0,0 +1,13 @@
+package MMIPlatform.MMIPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmiPlatformApplication.class, args);
+	}
+
+}
