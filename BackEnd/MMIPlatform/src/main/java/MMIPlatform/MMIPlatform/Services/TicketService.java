1a2b740e807219eafeadf6cd29f26f06e4cc86c0
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-03-28 21:52:24 +0100

    envoie du ticker

--- BackEnd/MMIPlatform/src/main/java/MMIPlatform/MMIPlatform/Services/TicketService.java
+++ BackEnd/MMIPlatform/src/main/java/MMIPlatform/MMIPlatform/Services/TicketService.java
@@ -4,12 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import MMIPlatform.MMIPlatform.Repositories.TicketRepository;
+import MMIPlatform.MMIPlatform.Models.Ticket;
 
 
 @Service
 public class TicketService {
     @Autowired
         private TicketRepository ticketRepository;
+
+    public Ticket saveTicket(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
+
+  
+    public Ticket findById(Long id) {
+        if(id == null){
+            throw new IllegalArgumentException("ID cannot be null");
+        }
+        return ticketRepository.findById(id).orElse(null);
+    }
 
 
 }
