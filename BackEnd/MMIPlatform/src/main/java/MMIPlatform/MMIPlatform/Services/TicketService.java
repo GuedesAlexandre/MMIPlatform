62f348f1287d317bc1bdf9296faf49d9e42f2250
Glenn Guillard <glenn.guillard.pros@gmail.com>
2024-03-28 21:49:38 +0100

    Continuation Ticket

--- BackEnd/MMIPlatform/src/main/java/MMIPlatform/MMIPlatform/Services/TicketService.java
+++ BackEnd/MMIPlatform/src/main/java/MMIPlatform/MMIPlatform/Services/TicketService.java
@@ -3,6 +3,7 @@ package MMIPlatform.MMIPlatform.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import MMIPlatform.MMIPlatform.Models.Ticket;
 import MMIPlatform.MMIPlatform.Repositories.TicketRepository;
 
 
@@ -11,5 +12,12 @@ public class TicketService {
     @Autowired
         private TicketRepository ticketRepository;
 
+
+        public Ticket findById(Long id) {
+            if(id == null){
+                throw new IllegalArgumentException("ID cannot be null");
+            }
+            return ticketRepository.findById(id).orElse(null);
+        }
 
 }
